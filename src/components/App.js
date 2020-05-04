a484e81fee0df03b78cca657dfc4fbe78d5e697f
AndrewDAntonio <github email address>
2020-05-04 09:02:49 -0400

    Routing and forms

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,45 @@
 import React from 'react';
-import '../App.css';
+import {Route, Switch} from 'react-router-dom'
 import Header from './Header.js';
 import BoardContainer from './BoardContainer.js';
 import PostContainer from './PostContainer.js';
+import LoginForm from './LoginForm.js'
+import SignupForm from './SignupForm.js'
 
 class App extends React.Component {
-  
+  state = {
+    page: 'signup'
+  }
+
+  getPage() {
+    switch (this.state.page) {
+      case "login":
+        return <LoginForm />
+      case "signup":
+        return <SignupForm />
+      default:
+        return <h1>404</h1>
+    }
+  }
   render() {
     
     return (
-    <div className="App">
-    test
-      <Header/>
-      <BoardContainer/>
-      <PostContainer/>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Switch>
+          <Route exact path='/login' render={(routeProps) => <LoginForm {...routeProps} />} />
+          <Route exact path='/signup' render={(routeProps) => <SignupForm {...routeProps} />} />
+          <Route exact path='/boards' render={(routeProps) => <BoardContainer {...routeProps} />} />
+          <Route exact path='/posts' render={(routeProps) => <PostContainer {...routeProps} />} />
+        </Switch>
+      </main>
+      
+    </>
     );
   }
 }
 
 export default App;
+
+

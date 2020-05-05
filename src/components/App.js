c104c1ab4901aa3949637d9f8edd016ab5b01122
mav-code <mavisansmembres@gmail.com>
2020-05-05 12:28:21 -0400

    comments displaying per board

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import {Route, Switch, withRouter} from 'react-router-dom'
 import Header from './Header.js';
 import BoardContainer from './BoardContainer.js';
 import PostContainer from './PostContainer.js';
+import CommentContainer from './CommentContainer.js';
 import LoginForm from './LoginForm.js'
 import SignupForm from './SignupForm.js'
 
@@ -23,12 +24,12 @@ handleUpdateCurrentUser = user => {
     <>
       <Header />
       <BoardContainer />
-      <PostContainer />
+      <CommentContainer />
       <main>
         <Switch>
           <Route exact path='/login' render={(routeProps) => <LoginForm handleUpdateCurrentUser={this.handleUpdateCurrentUser} {...routeProps} />} />
           <Route exact path='/signup' render={(routeProps) => <SignupForm handleUpdateCurrentUser={this.handleUpdateCurrentUser} {...routeProps} />} />
-          {/* first thing tomorrow <Route path='boards/:id' render={(routeProps) => <PostContainer {...routeProps} />} /> */}
+          <Route path='/boards' render={(routeProps) => <PostContainer {...routeProps} />} />
         </Switch>
       </main>
       
--- src/components/CommentContainer.js
+++ src/components/CommentContainer.js
@@ -1,17 +1,39 @@
 import React from 'react'
 import Comment from './Comment.js'
+import { withRouter } from "react-router"
 
 class CommentContainer extends React.Component {
+
+  state = {
+    comments: []
+  }
+
+  componentDidMount() {
+    console.log("in commentcontainer cdm 1", this.props)
+    fetch(`http://localhost:3000/comments/`)
+      .then(r => r.json())
+      .then(commentsArray => {
+        this.setState({
+          comments: commentsArray
+        })
+        console.log("in commentcontainer cdm 2", commentsArray)
+      })
+  }
+
+  thisPostsComments = () => {
+    ///// very janky line. must be better way
+    return this.state.comments.filter(comment => comment.post_id === parseInt(this.props.location.pathname[this.props.location.pathname.length - 1]))
+  }
   
     render() {
       
       return (
         <>
         This is where the comments go
-        <Comment/>
+        {this.thisPostsComments().map(comment => <Comment key={comment.id} comment={comment}/>)}
         </>
       )
     }
 }
 
-export default CommentContainer;
+export default withRouter(CommentContainer);

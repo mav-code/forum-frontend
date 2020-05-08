3bdbc469d84ecb4a98bef683fb0242b5742ee1c9
mav-code <mavisansmembres@gmail.com>
2020-05-08 09:37:25 -0400

    body of post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import {Route, Switch, withRouter} from 'react-router-dom'
 
 
 
 class Post extends React.Component {
 
     render() {
-      console.log("in post", this.props)
+      console.log("pathname", this.props.location.pathname.substring(1, 6))
+      let body
+      if(this.props.location.pathname.substring(1, 6) === "posts"){
+        body = <tr>
+          <td>{this.props.post.body}</td>
+          </tr>
+      }
       if(this.props.post){
       return (
         <>
@@ -14,6 +21,7 @@ class Post extends React.Component {
             <td>{this.props.post.user.username}</td>
             <td>{this.props.post.comments.length}</td>
           </tr>
+          {body}
           
         </>
       )} else {
@@ -22,4 +30,4 @@ class Post extends React.Component {
     }
 }
 
-export default Post;
+export default withRouter(Post);
